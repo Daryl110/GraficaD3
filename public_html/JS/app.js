4d9efae56df459de0fee725330657df3b995b80a
Daryl110 <daryl.8220@gmail.com>
2018-09-03 16:12:25 -0500

    Correccion de nulos

--- public_html/JS/app.js
+++ public_html/JS/app.js
@@ -5,35 +5,41 @@ var datos = [];
 function graficarEstudiantes() {
 
     d3.json('./Recursos/JSON/estudiantes.json', function (err, d) {
-        if (err) { console.log(err); return; };
+        if (err) {
+            console.log(err);
+            return;
+        }
+        ;
     }).then((e) => {
         datos = e.datos;
-        var margin = { top: 20, right: 20, bottom: 70, left: 40 },
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+        var margin = {top: 20, right: 20, bottom: 70, left: 40},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
 
         var dat = [];
 
         $.each(datos, function (index, estudiante) {
-            dat.push({
-                'semestre': estudiante.ultimoSemestreCursado === null ? '0000' : estudiante.ultimoSemestreCursado,
-                'anio': estudiante.ultimoAnioCursado === null ? '0000' : estudiante.ultimoAnioCursado
-            });
+            if (estudiante.ultimoSemestreCursado !== null) {
+                dat.push({
+                    'semestre': estudiante.ultimoSemestreCursado,
+                    'anio': estudiante.ultimoAnioCursado
+                });
+            }
         });
 
-        var dominio = [2007, 2014];
+        var dominio = [2007, 2018];
         var rango = [1, 9];
 
-        d3.select('svg')
-            .selectAll('div')
-            .data(dat)
-            .enter()
-            .append('div')
-            .style('width', function (d) {
-                return '15px';
-            })
-            .text(function (d) {
-                return d.anio;
-            });
+        d3.select('label')
+                .selectAll('div')
+                .data(dat)
+                .enter()
+                .append('div')
+                .style('width', function (d) {
+                    return '15px';
+                })
+                .text(function (d) {
+                    return d.anio;
+                });
     });
 }
